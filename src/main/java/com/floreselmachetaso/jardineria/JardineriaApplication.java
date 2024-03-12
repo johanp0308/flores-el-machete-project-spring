8f39d335aa248a8ad0823a4d9ff390ed5f3231f3
johanp0308 <sebasstian-johan1@hotmail.com>
2024-03-12 12:46:36 -0500

    feat: :tada: Init Commit

---
+++ src/main/java/com/floreselmachetaso/jardineria/JardineriaApplication.java
@@ -0,0 +1,14 @@
+package com.floreselmachetaso.jardineria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JardineriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JardineriaApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/floreselmachetaso/jardineria/JardineriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floreselmachetaso.jardineria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JardineriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
