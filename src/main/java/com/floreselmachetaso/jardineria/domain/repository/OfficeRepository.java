4762c6ccbcbffb00fca2555e4d52f7aa1d3fe4b1
Santiago24x <santiagomarquez.it@gmail.com>
2024-03-14 13:09:37 -0500

    feat: :sparkles: Add more repositories

---
+++ src/main/java/com/floreselmachetaso/jardineria/domain/repository/OfficeRepository.java
@@ -0,0 +1,10 @@
+package com.floreselmachetaso.jardineria.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.floreselmachetaso.jardineria.persistence.entities.Office;
+
+@Repository
+public interface OfficeRepository extends JpaRepository<Office, String>  {
+}
