d133727a83541f3767b3f67b3139914915310d13
johanp0308 <sebastian-johan1@hotmail.com>
2024-03-14 12:03:28 -0500

    fix: :ambulance: Change big

--- src/main/java/com/floreselmachetaso/jardineria/domain/repository/CustomerRepository.java
+++ src/main/java/com/floreselmachetaso/jardineria/domain/repository/CustomerRepository.java
@@ -1,9 +1,9 @@
-package com.floreselmachetaso.jardineria.persistence.repository;
+package com.floreselmachetaso.jardineria.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.floreselmachetaso.jardineria.domain.entities.Customer;
+import com.floreselmachetaso.jardineria.persistence.entities.Customer;
 
 import java.util.Optional;
 
--- src/main/java/com/floreselmachetaso/jardineria/persistence/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.floreselmachetaso.jardineria.persistence.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.floreselmachetaso.jardineria.domain.entities.OrderDetail;
-import com.floreselmachetaso.jardineria.domain.entities.OrderDetailPK;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, OrderDetailPK> {
-}
