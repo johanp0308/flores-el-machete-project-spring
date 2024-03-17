ba18b73ffc68b3b3e4eb1c67e37b4ea5524ba26c
johanp0308 <sebastian-johan1@hotmail.com>
2024-03-17 13:49:49 -0500

    feat: :sparkles: Adding multi-table queries to CustomerRepository

--- src/main/java/com/floreselmachetaso/jardineria/domain/repository/CustomerRepository.java
+++ src/main/java/com/floreselmachetaso/jardineria/domain/repository/CustomerRepository.java
@@ -25,4 +25,42 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
      @Query("SELECT c.* FROM cliente c JOIN empleado e ON c.codigo_empleado_rep_ventas = e.codigo_empleado WHERE c.ciudad = 1? AND e.codigo_empleado IN (2?, 3?)")
      List<Object[]> findAllCustomerbyCityWRepresentIdOrId(String city,int repre1, int repre2);
+
+    /*
+    * Obtén un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas.
+    */
+    @Query("Obtén un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas.")
+    List<Object[]> findAllCustomerWSalesRepres();
+
+    /*
+     * Muestra el nombre de los clientes que hayan realizado pagos junto con el nombre de sus representantes de ventas.
+     */
+    @Query("SELECT cli.nombre_cliente, emp.nombre AS nombre_representante, emp.apellido1 AS apellido_representante FROM cliente AS cli JOIN pago AS p ON cli.codigo_cliente = p.codigo_cliente JOIN empleado AS emp ON cli.codigo_empleado_rep_ventas = emp.codigo_empleado")
+    List<Object[]> findAllCustomerWSalesRepresPay();
+
+    /*
+     * Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+     */
+    @Query("SELECT c.nombre_cliente, e.nombre AS nombre_representante, e.apellido1 AS apellido_representante, o.ciudad FROM cliente AS c JOIN pago AS p ON c.codigo_cliente = p.codigo_cliente JOIN empleado AS e ON c.codigo_empleado_rep_ventas = e.codigo_empleado JOIN oficina AS o ON e.codigo_oficina = o.codigo_oficina")
+
+    List<Object[]> findAllCustomerWSalesRepresPayWCity();
+
+    /*
+     * Devuelve el nombre de los clientes que no hayan hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+     */
+    @Query("SELECT c.nombre_cliente, e.nombre AS nombre_representante, e.apellido1 AS apellido_representante, o.ciudad FROM cliente AS c JOIN empleado AS e ON c.codigo_empleado_rep_ventas = e.codigo_empleado JOIN oficina AS o ON e.codigo_oficina = o.codigo_oficina WHERE c.codigo_cliente NOT IN (SELECT codigo_cliente FROM pago)")
+    List<Object[]> findAllCustomerWSalesRepresNoPayWCity();
+
+    /*
+     * Lista la dirección de las oficinas que tengan clientes en Fuenlabrada.
+     */
+    @Query("SELECT DISTINCT o.linea_direccion1, o.linea_direccion2, o.ciudad, o.pais FROM oficina AS o JOIN empleado AS e ON o.codigo_oficina = e.codigo_oficina JOIN cliente AS c ON e.codigo_empleado = c.codigo_empleado_rep_ventas WHERE c.ciudad = ?")
+    List<Object[]> findAllCustomerAddressWCity(String city);
+
+    /*
+     * Devuelve el nombre de los clientes y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+     */
+    @Query("SELECT c.nombre_cliente, e.nombre AS nombre_representante, e.apellido1 AS apellido_representante, o.ciudad FROM cliente AS c JOIN empleado AS e ON c.codigo_empleado_rep_ventas = e.codigo_empleado JOIN oficina AS o ON e.codigo_oficina = o.codigo_oficina")
+    List<Object[]> findAllCustomerAndSalesRepresWCity();
+
 }
