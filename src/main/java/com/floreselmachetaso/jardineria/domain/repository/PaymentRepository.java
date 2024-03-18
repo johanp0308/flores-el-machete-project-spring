6b9e36db208db315f4abd1b234068acb31cc11af
johanp0308 <sebastian-johan1@hotmail.com>
2024-03-18 07:13:00 -0500

    feat: :sparkles: Remove import innecesary

--- src/main/java/com/floreselmachetaso/jardineria/domain/repository/PaymentRepository.java
+++ src/main/java/com/floreselmachetaso/jardineria/domain/repository/PaymentRepository.java
@@ -2,7 +2,6 @@ package com.floreselmachetaso.jardineria.domain.repository;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
