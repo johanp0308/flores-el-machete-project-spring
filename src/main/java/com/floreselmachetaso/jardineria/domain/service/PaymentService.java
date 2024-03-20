b4ba52302a9e03f23cec046e20a4dc2168cef7d2
johanp0308 <sebastian-johan1@hotmail.com>
2024-03-20 11:40:49 -0500

    feat: :sparkles: Change Service

--- src/main/java/com/floreselmachetaso/jardineria/domain/service/PaymentService.java
+++ src/main/java/com/floreselmachetaso/jardineria/domain/service/PaymentService.java
@@ -1,16 +1,17 @@
 package com.floreselmachetaso.jardineria.domain.service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PaymentService {
 
-    List<Object[]> averagePayYear();
+    List<Map<String,Object>> averagePayYear();
 
-    List<Object[]> endDatePayForCustomer();
+    List<Map<String,Object>> endDatePayForCustomer();
 
-    List<Object[]> amountCustomerDiffOrder();
+    List<Map<String,Object>> amountCustomerDiffOrder();
 
-    List<Object[]> sumAmountCustomerDiffOrder();
+    List<Map<String,Object>> sumAmountCustomerDiffOrder();
 
-    List<Object[]> topProductsMoreSales();
+    List<Map<String,Object>> topProductsMoreSales();
 }
